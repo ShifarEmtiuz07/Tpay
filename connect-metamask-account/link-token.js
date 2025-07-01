3de89c9eb55240db11bcc069be1c6dfcf257a75f
A. B. M. Shifar Emtiuz <emitiazshifar007@gmail.com>
2025-07-01 17:51:44 +0600

    work on docker and write README.md file

--- connect-metamask-account/link-token.js
+++ connect-metamask-account/link-token.js
@@ -1,14 +1,13 @@
-// link-token.js
 import { ethers } from "ethers";
 
 // Replace with your Infura or Alchemy API URL
 const RPC_URL = "https://mainnet.infura.io/v3/374f2f5a31ad42e28611101c186b7575";;
 
-// Your test wallet address
-const walletAddress = "0x0650ecDcE71157B9C73ef3eD9d9c7EaDcf8aEb94"; // Example wallet
 
-// USDT contract on Ethereum Mainnet
-const tokenAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const walletAddress = "0x0650ecDcE71157B9C73ef3eD9d9c7EaDcf8aEb94"; 
+
+
+const tokenAddress ="0xdAC17F958D2ee523a2206206994597C13D831ec7"                   //usdt:"0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
 const provider = new ethers.JsonRpcProvider(RPC_URL);
 
@@ -17,6 +16,7 @@ const abi = [
   "function symbol() view returns (string)",
   "function decimals() view returns (uint8)",
   "function balanceOf(address) view returns (uint)",
+  "function totalSupply() view returns (uint)"
 ];
 
 const fetchRealTokenBalance = async () => {
@@ -25,12 +25,16 @@ const fetchRealTokenBalance = async () => {
   const name = await contract.name();
   const symbol = await contract.symbol();
   const decimals = await contract.decimals();
+  
   const balance = await contract.balanceOf(walletAddress);
+  const totalSupply = await contract.totalSupply();
 
   console.log("Token:", name, `(${symbol})`);
  const divisor = BigInt(10) ** BigInt(decimals);
   const humanBalance = Number(balance) / Number(divisor);
   console.log("Balance:", humanBalance);
+  const humanSupply = Number(totalSupply) / Number(divisor);
+  console.log(humanSupply);
 };
 
 fetchRealTokenBalance();
--- connect-metamask-account/sign-login.js
+++ connect-metamask-account/sign-login.js
@@ -1,11 +1,11 @@
-//const { Wallet } = require('ethers');
+
 import { Wallet } from 'ethers';
 
-// ⚠️ Replace with your test MetaMask private key
-const privateKey = '1ec725c345371801f811e09f868e98c6c9683dbea15184c9bd71b07bdc8798d4'; 
 
-// Replace with your dev wallet address (corresponding to the private key)
-const walletAddress = '0x0650ecDcE71157B9C73ef3eD9d9c7EaDcf8aEb94';
+const privateKey = '27b823f376179fefdeacff1afe777a59fc4031753f4049de8b52d0356672269c'; 
+
+
+const walletAddress = '0x6C5D1aF464097c88cECb944edB0b83A5D97AdE17';
 
 // This nonce must match the one from your backend for this wallet
  const nonce = `Login to Tpay at ${new Date().toISOString()}`; // same as what your backend stores
